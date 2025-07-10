951353a9fb13e17e6c0ed48ef7b1fc2452d4940f
Alpha-ZT <zerayfiraol@gmail.com>
2025-07-10 13:56:54 +0300

    simple_interest.py

---
+++ python_introduction/simple_interest.py
@@ -0,0 +1,7 @@
+principal = 1000       # Initial investment ($1000)
+rate = 0.05            # Annual interest rate (5%)
+time = 3               # Investment duration (3 years)
+
+interest = principal * rate * time
+
+print(f"The simple interest is: {interest}")
